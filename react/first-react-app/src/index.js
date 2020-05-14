95dc8379fe5925bc4a371bc8c36db04ed43d2a59
Tanya <thaiwan7@gmail.com>
2020-05-13 17:58:29 -0700

    sending was added. minor fixes.

--- react/first-react-app/src/index.js
+++ react/first-react-app/src/index.js
@@ -9,21 +9,23 @@ import {
   Button,
   CssBaseline,
 } from '@material-ui/core';
+import axios from 'axios';
 
 
 const onSubmit = async values => {
-  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-  await sleep(300);
-  window.alert(JSON.stringify(values, 0, 2));
-};
 
+  axios.post('https://uha140ohua.execute-api.us-east-1.amazonaws.com/test/email/', JSON.stringify(values))
+      .then((result) => { window.alert(JSON.stringify(result.data)) },
+          (error) => { window.alert(error) }
+      );
+};
 const validate = values => {
   const errors = {};
-  if (!values.firstName) {
-    errors.firstName = 'Required';
+  if (!values.name) {
+    errors.name = 'Required';
   }
-  if (!values.lastName) {
-    errors.lastName = 'Required';
+  if (!values.message) {
+    errors.message = 'Required';
   }
   if (!values.email) {
     errors.email = 'Required';
@@ -34,75 +36,74 @@ const validate = values => {
 function App() {
   return (
       <div style={{ padding: 16, margin: 'auto', maxWidth: 600 }}>
-<CssBaseline />
-  <Typography variant="h4" align="center" component="h1" gutterBottom>
-  Email Sending Form
-  </Typography>
-  <Form
-  onSubmit={onSubmit}
-  validate={validate}
-  render={({ handleSubmit, reset, submitting, pristine, values }) => (
-  <form onSubmit={handleSubmit} noValidate>
-  <Paper style={{ padding: 16 }}>
-<Grid container alignItems="flex-start" spacing={2}>
-      <Grid item xs={6}>
-      <Field
-  fullWidth
-  required
-  name="firstName"
-  component={TextField}
-  type="text"
-  label="First Name"
-      />
-      </Grid>
-      <Grid item xs={6}>
-      <Field
-  fullWidth
-  required
-  name="lastName"
-  component={TextField}
-  type="text"
-  label="Last Name"
-      />
-      </Grid>
-      <Grid item xs={12}>
-      <Field
-  name="email"
-  fullWidth
-  required
-  component={TextField}
-  type="email"
-  label="Email"
-      />
-      </Grid>
-      <Grid item style={{ marginTop: 16 }}>
-<Button
-  type="button"
-  variant="contained"
-  onClick={reset}
-  disabled={submitting || pristine}
->
-  Reset
-  </Button>
-  </Grid>
-  <Grid item style={{ marginTop: 16 }}>
-<Button
-  variant="contained"
-  color="primary"
-  type="submit"
-  disabled={submitting}
-      >
-      Submit
-      </Button>
-      </Grid>
-      </Grid>
-      </Paper>
-      <pre>{JSON.stringify(values, 0, 2)}</pre>
-      </form>
-)}
-  />
-  </div>
-);
+        <CssBaseline />
+        <Typography variant="h4" align="center" component="h1" gutterBottom>
+          Email Sending Form
+        </Typography>
+        <Form
+            onSubmit={onSubmit}
+            validate={validate}
+            render={({ handleSubmit, reset, submitting, pristine, values }) => (
+                <form onSubmit={handleSubmit} noValidate>
+                  <Paper style={{ padding: 16 }}>
+                    <Grid container alignItems="flex-start" spacing={2}>
+                      <Grid item xs={6}>
+                        <Field
+                            fullWidth
+                            required
+                            name="name"
+                            component={TextField}
+                            type="text"
+                            label="Name"
+                        />
+                      </Grid>
+                      <Grid item xs={6}>
+                        <Field
+                            fullWidth
+                            required
+                            name="email"
+                            component={TextField}
+                            type="email"
+                            label="Email"
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Field
+                            name="message"
+                            fullWidth
+                            required
+                            component={TextField}
+                            type="text"
+                            label="Message"
+                        />
+                      </Grid>
+                      <Grid item style={{ marginTop: 16 }}>
+                        <Button
+                            type="button"
+                            variant="contained"
+                            onClick={reset}
+                            disabled={submitting || pristine}
+                        >
+                          Reset
+                        </Button>
+                      </Grid>
+                      <Grid item style={{ marginTop: 16 }}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            disabled={submitting}
+                        >
+                          Submit
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </Paper>
+                </form>
+            )}
+        />
+      </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
